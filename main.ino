83b416d3c279ef6a16a501738f669ec54ec4c694
Victor Taksheyev <victortaksheyev@gmail.com>
2020-03-20 18:04:20 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -59,6 +59,8 @@ void setup() {
     }
   }
 
+  SerialPort.println("test");
+
   imu.setSensors(INV_XYZ_GYRO); // Enable gyroscope only
   imu.setGyroFSR(DPS_MAX); // Set gyro to 2000 dps
 
@@ -66,43 +68,109 @@ void setup() {
               DMP_FEATURE_SEND_CAL_GYRO,// Send cal'd gyro values
               10);                   // Set DMP rate to 10 Hz
 
-  delay(8000);
+//  delay(5000);
 //  calibrated = detectCalibration();
 }
 
 float av_old  = 0;
 float gyroZ;
+long oldPosition  = -999;
+
+// function declarations
+bool detectCalibration();
+float calcCorrection(float& av_old);
+float performCorrection(float correction_av);
+float getPosition();
+float getGyroZ(bool log);
+
 
 void loop() {
     if (!calibrated)
       calibrated = detectCalibration();
-    
+//    
     if (calibrated) {
       if (calibration_sequence) {
         calibration_sequence = false;
       }
-      if (imu.fifoAvailable() ) {
-      // Use dmpUpdateFifo to update the ax, gx, mx, etc. values
-      if (imu.dmpUpdateFifo() == INV_SUCCESS) {
-//        SerialPort.print(calcCorrection(av_old));
-        float correction_av = calcCorrection(av_old);
-        performCorrection(correction_av);
 
+      
+      if (imu.fifoAvailable() ) {
+//      // Use dmpUpdateFifo to update the ax, gx, mx, etc. values
+      if (imu.dmpUpdateFifo() == INV_SUCCESS) {
+
+        float curr_gyro_z = getGyroZ(false);
+        float rocket_angular_radian = deg_to_rad(abs(curr_gyro_z));
+        float motor_duty = neededMotorDuty(rocket_angular_radian);
+        motor.setSpeed(motor_duty);
+        if (curr_gyro_z < 0) {
+          // rotate the motor in the opposite direction, based on the value of the gyroZ
+            motor.forward();
+            delay(10);
+        } else {
+            motor.backward();
+            delay(10);
+        }
+        
+//        SerialPort.print(calcCorrection(av_old));
+//        float correction_av = calcCorrection(av_old);
+//        performCorrection(correction_av);
       }
     }
-
-    delay(10);
-
+//
+//    delay(10);
+//
     }
     
+}
+
+float neededMotorDuty(float rocket_angular_radian) {
+  float constant = 1.0;
+  float motor_angular_radian = constant * rocket_angular_radian;
+
+  float motor_angular_revolutions = motor_angular_radian / (2 * PI);
+
+  // calculating motor voltage
+  float numerator = log(motor_angular_revolutions) - log(0.327);
+  float motor_duty = numerator / (0.01275);
+
+  if (motor_angular_revolutions <= 0.33) {
+      motor_duty = 0;
+  }
+
+  SerialPort.print(millis());
+  SerialPort.print("   Motor duty: ");
+  SerialPort.println(motor_duty);
+
+  return motor_duty + 20;
+
+}
+
+float getPosition() {
+  long newPosition = encoder.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    unsigned long time = millis();
+    SerialPort.print(time);
+    SerialPort.print(",");
+    SerialPort.println(newPosition);
+  }
+  
+}
+
+// return radians per second
+float deg_to_rad(float degree) {
+    float numerator = degree*2*PI;
+    return numerator / 360;         // radians/sec
 }
 
 // reads gyroZ from the sensor and logs it to the serial 
 float getGyroZ(bool log) {
   float gyroZ = imu.calcGyro(imu.gz);
   
-  if (log)
+  if (log) {
+    SerialPort.print("Gyro Z: ");
     SerialPort.println(gyroZ);
+  }
 
   return gyroZ;
 }
@@ -128,6 +196,7 @@ float calcCorrection(float& av_old)
 float performCorrection(float correction_av) {
   float motor_av_t = map(abs(correction_av), DPS_MIN, DPS_MAX, MOTOR_MIN, MOTOR_MAX);
   motor.setSpeed(motor_av_t); // set to computed gyroscope angular velocity
+  // rotate the motor faster and faster until the speed read by the ... isn't 0
   if (correction_av > 0) {
     // while (motor_av_r < motor_av_t)
     motor.backward();        // continue moving forwards while motor_av_r(eal) != motor_av_t(heoretical)
